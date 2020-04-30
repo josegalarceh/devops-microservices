586fc08f2346e520405b83b28a4201c851cd36fb
josegalarceh <ubuntu@ip-10-0-0-202.us-east-2.compute.internal>
2020-04-30 05:00:28 +0000

    Task 2: Run a Container

--- test.sh
+++ test.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
-
+pwd
+hostname
+# Run flask app
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build -t josegalarceh-app:v0.1.0 .
 
 # Step 2: 
 # List docker images
-docker image ls
-
-# Step 3: 
-# Run flask app
-docker run -p 8000:80 josegalarceh-app:v0.1.0
