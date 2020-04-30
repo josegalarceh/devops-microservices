b3dbc1959487473b098e0128bf31c7dcedd259d8
jose manuel galarce <jmgalarceh@achs.cl>
2020-04-30 03:53:14 -0300

    Task 4: Upload the Docker Image

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=josegalarceh/josegalarceh-app:v0.1.0
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username josegalarceh
+docker tag josegalarceh-app:v0.1.0 $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
