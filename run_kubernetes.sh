8a2a30bbeff5f3e051487f7844cbfde16d3913a7
jose manuel galarce <jmgalarceh@achs.cl>
2020-05-04 01:49:10 -0300

    Task 6: Deploy with Kubernetes and Save Output Logs

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=josegalarceh/josegalarceh-app:v0.1.0
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment app --image=josegalarceh-app:v0.1.0
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl expose deployment app --type=LoadBalancer --port=8000 --target-port=80
